d4dd6c9d93f6e5053fcc4e52dde7791469d38288
priyanka586 <priyaketha4@gmail.com>
2025-07-25 11:14:56 +0530

    Add files via upload

---
+++ Nested.java
@@ -0,0 +1,31 @@
+class Nested
+{
+public static void main(String args[])c
+{
+ int score=80;
+if(score>=80)
+System.out.println("A");
+}
+else
+{
+if(score>=70)
+System.out.println("B");
+}
+else
+{
+if(score>=60)
+System.out.println("c");
+}
+else
+{
+if(score>=50)
+System.out.println("D");
+}
+else
+{
+if(score>=40)
+System.out.println("E");
+}
+}
+
+
